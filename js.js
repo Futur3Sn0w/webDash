f386e4234eb25773e2f058c96c3f23d0c1129679
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2022-06-20 15:29:34 -0400

    -

--- js.js
+++ js.js
@@ -1,4 +1,18 @@
 var settingsBox = document.getElementById("settingsBox");
+var screenOverlay = document.getElementById("screenOverlay");
+var colorPicker = document.getElementById("bgColorPicker");
+
+function wallSet() {
+    if (localStorage.getItem('bgType') == "bgColor") {
+        document.body.style.backgroundImage = "unset";
+        document.body.style.backgroundColor = localStorage.getItem('bgSelColor');
+        document.getElementById('bgColor').checked = true;
+    } else if (localStorage.getItem('bgType') == "bgUnsplash") {
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/?wallpaper')";
+        document.body.style.backgroundColor = "transparent";
+        document.getElementById('bgUnsplash').checked = true;
+    }
+}
 
 function openTab(evt, tabName) {
     var i;
@@ -18,8 +32,22 @@ function openTab(evt, tabName) {
 
 function openSettings() {
     settingsBox.style.display = "flex";
+    screenOverlay.style.display = "block";
 }
 
 function closeSettings() {
     settingsBox.style.display = "none";
+    screenOverlay.style.display = "none";
+}
+
+function bgType(radio){
+    localStorage.setItem("bgType", radio);
+    wallSet();
+}
+
+
+colorPicker.addEventListener("change", watchColorPicker, false);
+function watchColorPicker(event) {
+    localStorage.setItem('bgSelColor', event.target.value);
+    document.body.style.backgroundColor = localStorage.getItem('bgSelColor');
 }
